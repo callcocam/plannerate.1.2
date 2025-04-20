fb1e33324675b8baaf29b5db8fa49dfd7828210c
callcocam <callcocam@gmail.com>
2025-04-19 23:43:54 -0300

    fix(api): atualizar rotas e remover métodos obsoletos no GondolaController
    - Adicionada a rota PUT para salvar planogramas no arquivo `api.php`, permitindo a atualização de dados de forma mais eficiente.
    - Removidos métodos obsoletos do `GondolaController`, como `reorder`, `destroyShelf`, `scaleFactor` e `alignment`, simplificando a lógica do controlador.
    - Mantidas as funcionalidades existentes, assegurando a integridade do código e a experiência do usuário.

--- routes/api.php
+++ routes/api.php
@@ -14,69 +14,13 @@ Route::middleware(['api', 'auth:sanctum'])
     ->group(function () {
 
         Route::get('plannerate/{id}', [PlannerateController::class, 'show'])->name('plannerate.show');
+        
         Route::put('plannerate/{planogram}', [PlannerateController::class, 'save'])->name('plannerate.save');
 
         Route::resource('gondolas', GondolaController::class);
-        Route::post('gondolas/{gondola}/sections/reorder', [GondolaController::class, 'reorder'])
-            ->name('gondolas.sections.reorder');
-
-        Route::post('gondolas/{gondola}/scaleFactor', [GondolaController::class, 'scaleFactor'])
-            ->name('gondolas.scaleFactor');
-
-        Route::post('gondolas/{gondola}/alignment', [GondolaController::class, 'alignment'])
-            ->name('gondolas.alignment');
 
         Route::resource('sections', SectionController::class);
-
-        Route::post('sections/{gondola}/shelves/reorder', [SectionController::class, 'updateInvertOrder'])
-            ->name('sections.updateInvertOrder');
-
-        Route::post('sections/{section}/alignment', [SectionController::class, 'alignment'])
-            ->name('sections.alignment');
-
-        Route::post('sections/{section}/inverterShelves', [SectionController::class, 'inverterShelves'])
-            ->name('sections.invert');
-
-        Route::resource('shelves', ShelfController::class)
-            ->only(['index', 'show', 'store', 'update', 'destroy']);
-
-        // Rota para excluir prateleira pelo GondolaController
-        Route::delete('gondolas/shelves/{id}', [GondolaController::class, 'destroyShelf'])
-            ->name('gondolas.shelves.destroy');
-
-
-
-
-
-        Route::resource('segments', SegmentController::class)
-            ->only(['index', 'show', 'store', 'update', 'destroy']);
-
-        Route::put('segments/{shelf}/reorder', [SegmentController::class, 'reorder'])
-            ->name('segments.reorder');
-
-        Route::put('segments/{segment}/transfer', [SegmentController::class, 'transfer'])
-            ->name('segments.transfer');
-
-        Route::post('shelves/{shelf}/segments', [ShelfController::class, 'segment'])
-            ->name('shelves.segments');
-
-        Route::post('shelves/{shelf}/segments/copy', [ShelfController::class, 'segmentCopy'])
-            ->name('shelves.segments.copy');
-
-        Route::patch('shelves/{shelf}/transfer', [ShelfController::class, 'transfer'])
-            ->name('shelves.transfer');
-
-
-        Route::post('shelves/{shelf}/alignment', [ShelfController::class, 'alignment'])
-            ->name('shelves.alignment');
-
-        Route::post('shelves/{shelf}/inverterSegments', [ShelfController::class, 'inverterSegments'])
-            ->name('shelves.invert');
-
-        Route::post('shelves/{shelf}/updatePosition', [ShelfController::class, 'updatePosition'])
-            ->name('shelves.updatePosition');
-
-
-        Route::resource('layers',  LayerController::class)
-            ->only(['index', 'show', 'store', 'update', 'destroy']);
+        Route::resource('shelves', ShelfController::class);
+        Route::resource('segments', SegmentController::class);
+        Route::resource('layers', LayerController::class);
     });
