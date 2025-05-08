dd3d072a1dad6142bec8bd96cb6de4a9961e1e11
callcocam <callcocam@gmail.com>
2025-05-08 02:21:45 -0300

    ts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,7 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    @vite([ 'resources/js/plannerate.js'])
+    @vite([ 'resources/js/plannerate.ts'])
 </head>
 
 <body class="font-sans antialiased">
