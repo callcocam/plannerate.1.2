7565b52581812d1a4c21a8d7753c2f0025d66fe2
alessandro <alessandrocampos224@gmail.com>
2025-07-13 00:49:43 -0300

    feat: Adicionar ícones de atalho e favicon à interface
    - Incluídos links para o favicon e ícone de toque da Apple no cabeçalho da aplicação, melhorando a identidade visual e a experiência do usuário ao acessar a aplicação em dispositivos móveis.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,6 +33,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    
+    <link rel="icon" href="/img/favicon.jpg" sizes="any">
+    <link rel="icon" href="/img/favicon.jpg" type="image/jpg">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     @vite([ 'resources/js/plannerate.ts'])
