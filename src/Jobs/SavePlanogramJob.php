f6ac5d5e28fb306fce262de5bd47bc9fdc2a21e5
callcocam <callcocam@gmail.com>
2025-11-12 17:14:54 -0300

    fix: Add job retry configuration and sequential processing for SavePlanogramJob

--- src/Jobs/SavePlanogramJob.php
+++ src/Jobs/SavePlanogramJob.php
@@ -21,11 +21,28 @@ class SavePlanogramJob implements ShouldQueue
     use Queueable;
 
     /**
+     * Número de tentativas em caso de falha
+     */
+    public $tries = 3;
+
+    /**
+     * Número máximo de exceções não tratadas permitidas
+     */
+    public $maxExceptions = 3;
+
+    /**
+     * Tempo de espera entre tentativas (em segundos)
+     */
+    public $backoff = [1, 5, 10];
+
+    /**
      * Create a new job instance.
      */
     public function __construct(public $request, public Planogram $planogram, public $user = null)
     {
-        //
+        // Garante que jobs do mesmo planograma processem sequencialmente
+        // Isso previne deadlocks ao evitar múltiplos jobs modificando os mesmos registros
+        $this->onQueue("planogram-{$planogram->id}");
     }
 
     /**
